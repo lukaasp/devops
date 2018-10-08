667b08ea85443ea1eaab2034dc671710a0899449
Lukas Parobok <lukaasp@users.noreply.github.com>
2018-10-08 20:38:21 +0200

    test

--- kops_alt.sh
+++ kops_alt.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-apt-get update
-apt-get install -o Dpkg::Options::="--force-confold" -y nvidia-docker2=2.0.3+docker17.03.2-1 nvidia-container-runtime=2.0.0+docker17.03.2-1
-tee /etc/docker/daemon.json <<EOF
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+&& distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+&& curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list \
+&& apt-get update \
+&& apt-get install -o Dpkg::Options::="--force-confold" -y nvidia-docker2=2.0.3+docker17.03.2-1 nvidia-container-runtime=2.0.0+docker17.03.2-1 \
+&& tee /etc/docker/daemon.json <<EOF
 {
     "default-runtime": "nvidia",
     "runtimes": {
